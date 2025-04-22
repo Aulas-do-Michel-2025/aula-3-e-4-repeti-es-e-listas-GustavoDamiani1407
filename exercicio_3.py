699006ae72b7e9489df5844088c50d1acdcbf3ae
GustavoDamiani1407 <gustavodamiani1407@gmail.com>
2025-04-22 15:12:32 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -30,7 +30,24 @@ Ambas
 """
 
 # Código para pegar as listas de input
-primeira_lista = [*map(int, input("Digite a sua primeira lista (separando os números por vírgula): ").split(","))]
-segunda_lista = [*map(int, input("Digite a sua segunda lista (separando os números por vírgula): ").split(","))]
+#primeira_lista = [*map(int, input("Digite a sua primeira lista (separando os números por vírgula): ").split(","))]
+#segunda_lista = [*map(int, input("Digite a sua segunda lista (separando os números por vírgula): ").split(","))]
 
 # Fazer a partir daqui
+primeira_lista = [*map(int, input("Digite a sua primeira lista (separando os números por vírgula): ").split(","))]
+segunda_lista = [*map(int, input("Digite a sua segunda lista (separando os números por vírgula): ").split(","))]
+maior_1 = 1
+maior_2 = 1
+for i in primeira_lista:
+    if i > maior_1:
+        maior_1 = i
+for j in segunda_lista:
+    if j > maior_2:
+        maior_2 = j
+
+if maior_1 > maior_2:
+    print('Primeira')
+elif maior_2 > maior_1:
+    print('Segunda')
+elif maior_1 == maior_2:
+    print('Ambas')
